3824f0a53906a2a06b820905556896624752d74d
chris <chris@DESKTOP-BA0TJJU>
2018-11-09 10:35:03 -0500

    Added title

--- src/airline/reservation/system/FXMLDocumentController.java
+++ src/airline/reservation/system/FXMLDocumentController.java
@@ -7,7 +7,6 @@ package airline.reservation.system;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -18,14 +17,11 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
-    private Label label;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private Label title;
+    
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
