e1f231a6092d76c8ea0a4fc7ceed0a1548631d61
chris <chris@DESKTOP-BA0TJJU>
2018-11-26 12:32:49 -0500

    Added FXML skeleton to controller document

--- src/airline/reservation/system/FXMLDocumentController.java
+++ src/airline/reservation/system/FXMLDocumentController.java
@@ -10,15 +10,96 @@ package airline.reservation.system;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 
 public class FXMLDocumentController implements Initializable {
     
+    @FXML
+    private TextField flightNumTwo;
+
+    @FXML
+    private TextField DepCityOne;
+
+    @FXML
+    private TextField IdNumTwo;
+
+    @FXML
+    private TextField nameTwo;
+
+    @FXML
+    private TextField flightNumOne;
+
+    @FXML
+    private TextField AvailSeatsOne;
+
+    @FXML
+    private TextArea AllPassResGivenSix;
+
+    @FXML
+    private TextField seatNumTwo;
+
+    @FXML
+    private TextField enterFlightThree;
+
+    @FXML
+    private TextField DeptTimeOne;
+
+    @FXML
+    private TextArea allFlightInfoFour;
+
+    @FXML
+    private TextField DestCityOne;
+
+    @FXML
+    private TextField ArrTimeOne;
+
+    @FXML
+    private TextArea allPassResAllFlightFive;
+
+    @FXML
+    private TextField enterFlightNumSix;
+
+    @FXML
+    private TextField flightDateOne;
+
+    @FXML
+    private TextArea seatMapArea;
+
+    @FXML
+    void submitButtonOne(ActionEvent event) {
+
+    }
+
+    @FXML
+    void provideInfoTwo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void processFlightInfoTwo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showMapThree(ActionEvent event) {
+
+    }
+
+    @FXML
+    void displayPassResSix(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exitSeven(ActionEvent event) {
     
-   
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
